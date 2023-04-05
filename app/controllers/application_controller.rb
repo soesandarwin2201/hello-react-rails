6a5ac774915f3da67afec008d75a2f09eebc05c0
Soe Sandar Win <soesandarwin2201@gmail.com>
2023-04-05 16:50:52 +0630

    fix the linters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
- before_action :set_cors_header
+  before_action :set_cors_header
 
   def set_cors_header
     response.headers['Access-Control-Allow-Origin'] = 'http://127.0.0.1:3000'
